26932346941fa9d7ccdace0bc3fa2af5bd18552f
Dave Bennett <dmbennett@anderson.edu>
2015-01-17 19:20:22 -0500

    major overhaul to pages, added new pages, and wrapped content in a content wrapper for better window sizing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
